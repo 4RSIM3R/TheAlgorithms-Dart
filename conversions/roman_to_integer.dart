1aab7362938ce2cd63b8c99b23ab3ed364b00473
ilzam <ilzammulkhaq85@gmail.com>
2022-10-20 12:25:17 +0700

    Enhance roman_to_integer conversion

--- conversions/roman_to_integer.dart
+++ conversions/roman_to_integer.dart
@@ -1,11 +1,21 @@
 import 'package:test/test.dart';
 
+final mapRomanToValue = {
+  'I': 1,
+  'V': 5,
+  'X': 10,
+  'L': 50,
+  'C': 100,
+  'D': 500,
+  'M': 1000,
+};
+
 int romanToInteger(var s) {
   int ans = 0;
   for (int i = 0; i < s.length; i++) {
-    int num1 = value(s[i]);
+    int num1 = mapRomanToValue[s[i]] ?? 0;
     if (i + 1 < s.length) {
-      int num2 = value(s[i + 1]);
+      int num2 = mapRomanToValue[s[i + 1]] ?? 0;
       if (num1 < num2) {
         ans = ans + num2 - num1;
         i++;
@@ -17,23 +27,6 @@ int romanToInteger(var s) {
     }
   }
   return ans;
-}
-
-int value(var r) {
-  if (r == 'I')
-    return 1;
-  else if (r == 'V')
-    return 5;
-  else if (r == 'X')
-    return 10;
-  else if (r == 'L')
-    return 50;
-  else if (r == 'C')
-    return 100;
-  else if (r == 'D')
-    return 500;
-  else if (r == 'M') return 1000;
-  return 0;
 }
 
 void main() {
